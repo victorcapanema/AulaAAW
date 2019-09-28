62500d3c131a78f5cbe40b38e7a48583ac6cfe14
Victor Capanema <carvalho.capanema@gmail.com>
2019-09-28 11:03:15 -0300

    Exercicio 5 Calculadora

---
+++ calculadora/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="app">
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+
+export default {
+    components:{Calculator}
+}
+</script>
+
+<style>
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105 ,118), rgb(41, 46, 73));
+
+}
+
+* {
+    font-family: "RobotoMono", monospace;
+}
+
+body{
+    margin: 0;
+}
+
+@font-face{
+    font-family: "RobotoMono";
+    src: url("R:\WorkSpaces\CursoJS\vue\calculadora\src\fonts\RobotoMono-Thin.ttf");
+}
+</style>
