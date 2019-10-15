672bc833e7984bd651bce5afcfffc90c42b23743
Victor <47992324+victorcapanema@users.noreply.github.com>
2019-10-15 20:48:41 -0300

    Atividade 3
    Atividade 3

---
+++ Aula3/Catalogo.API/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+       
+        static Dictionary<int, Produto> produtos = new Dictionary<int, Produto>();
+        List<Produto> lista = new List<Produto>();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            lista = produtos.Values.ToList();
+            return lista;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            if(produtos.ContainsKey(id)){
+                return produtos[id];
+            } 
+           else{
+               return new Produto();
+           }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Produto value)
+        {
+             produtos.Add(value.Codigo,value);
+        }
+
+        // PUT api/values/5
+        // [HttpPut("{id}")]
+        [HttpPut]
+        public void Put([FromBody] Produto value)
+        {
+           produtos[value.Codigo]=value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            produtos.Remove(id);
+        }
+    }
+}
---
+++ Aula3/Catalogo.API/Produto.cs
@@ -0,0 +1,11 @@
+public class Produto{
+    private int codigo;
+    private string nome;
+    private double preco;
+
+    public int Codigo { get => codigo; set => codigo = value; }
+    public string Nome { get => nome; set => nome = value; }
+    public double Preco { get => preco; set => preco = value; }
+
+
+}
